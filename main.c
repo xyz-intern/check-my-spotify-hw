d790ddfb1ee84784602e1f42342ed2177c5fbd49
임시온 <80656700+dlatldhs@users.noreply.github.com>
2023-10-30 16:29:54 +0900

    fix serial data recevie & lcd print & function calling
    1. LCD function call on the top a main function
    2. when serial communication than data save and while loop camp back LCD printing

--- main.c
+++ main.c
@@ -69,95 +69,18 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
-
+void lcd_write_nibble(uint8_t nibble, uint8_t rs);
+void lcd_send_cmd(uint8_t cmd);
+void lcd_send_data(uint8_t data);
+void lcd_init();
+void lcd_write_string(char *str);
+void lcd_set_cursor(uint8_t row, uint8_t column);
+void lcd_clear(void);
+void lcd_backlight(uint8_t state);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
-  /*
-   * 4 bit - nibble data LCD Display
-   */
-  uint8_t data = nibble << D4_BIT;
-  data |= rs << RS_BIT;
-  data |= backlight_state << BL_BIT; // Include backlight state in data
-  data |= 1 << EN_BIT;
-  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
-  HAL_Delay(1);
-  data &= ~(1 << EN_BIT);
-  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
-}
-
-
-void lcd_send_cmd(uint8_t cmd)
-{
-  uint8_t upper_nibble = cmd >> 4;
-  uint8_t lower_nibble = cmd & 0x0F;
-  lcd_write_nibble(upper_nibble, 0);
-  lcd_write_nibble(lower_nibble, 0);
-  if (cmd == 0x01 || cmd == 0x02) {
-    HAL_Delay(2);
-  }
-}
-
-void lcd_send_data(uint8_t data)
-{
-  uint8_t upper_nibble = data >> 4;
-  uint8_t lower_nibble = data & 0x0F;
-  lcd_write_nibble(upper_nibble, 1);
-  lcd_write_nibble(lower_nibble, 1);
-}
-
-void lcd_init() {
-  HAL_Delay(50);
-  lcd_write_nibble(0x03, 0);
-  HAL_Delay(5);
-  lcd_write_nibble(0x03, 0);
-  HAL_Delay(1);
-  lcd_write_nibble(0x03, 0);
-  HAL_Delay(1);
-  lcd_write_nibble(0x02, 0);
-  lcd_send_cmd(0x28);
-  lcd_send_cmd(0x0C);
-  lcd_send_cmd(0x06);
-  lcd_send_cmd(0x01);
-  HAL_Delay(2);
-}
-
-void lcd_write_string(char *str) {
-  while (*str) {
-    lcd_send_data(*str++);
-  }
-}
-
-void lcd_set_cursor(uint8_t row, uint8_t column) {
-    uint8_t address;
-    switch (row) {
-        case 0:
-            address = 0x00;
-            break;
-        case 1:
-            address = 0x40;
-            break;
-        default:
-            address = 0x00;
-    }
-    address += column;
-    lcd_send_cmd(0x80 | address);
-}
-
-void lcd_clear(void) {
-	lcd_send_cmd(0x01);
-    HAL_Delay(2);
-}
-
-void lcd_backlight(uint8_t state) {
-  if (state) {
-    backlight_state = 1;
-  } else {
-    backlight_state = 0;
-  }
-}
 
 /* USER CODE END 0 */
 
@@ -198,7 +121,6 @@ int main(void)
 
   char *text = "test";
   char int_to_str[10] = {'a','b','t','e','s','t'};
-  int count=0;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -230,7 +152,6 @@ int main(void)
 		  strncpy(int_to_str,(const char*)UserRxBufferFS,len);
 //		  strcat((char *)UserTxBufferFS,"\r\n"); // 끝에 문자열 추가하는 거
 	  	  CDC_Transmit_FS((uint8_t*)UserTxBufferFS, strlen((const char*)UserTxBufferFS)); // 보내기
-
 	  	  // memory init
 	  	  memset(UserRxBufferFS, 0, sizeof(UserRxBufferFS)); // 초기화
 	  	  memset(UserTxBufferFS, 0, sizeof(UserTxBufferFS)); // 초기화
@@ -364,6 +285,89 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
+  /*
+   * 4 bit - nibble data LCD Display
+   */
+  uint8_t data = nibble << D4_BIT;
+  data |= rs << RS_BIT;
+  data |= backlight_state << BL_BIT; // Include backlight state in data
+  data |= 1 << EN_BIT;
+  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
+  HAL_Delay(1);
+  data &= ~(1 << EN_BIT);
+  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
+}
+
+void lcd_send_cmd(uint8_t cmd)
+{
+  uint8_t upper_nibble = cmd >> 4;
+  uint8_t lower_nibble = cmd & 0x0F;
+  lcd_write_nibble(upper_nibble, 0);
+  lcd_write_nibble(lower_nibble, 0);
+  if (cmd == 0x01 || cmd == 0x02) {
+    HAL_Delay(2);
+  }
+}
+
+void lcd_send_data(uint8_t data)
+{
+  uint8_t upper_nibble = data >> 4;
+  uint8_t lower_nibble = data & 0x0F;
+  lcd_write_nibble(upper_nibble, 1);
+  lcd_write_nibble(lower_nibble, 1);
+}
+
+void lcd_init() {
+  HAL_Delay(50);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(5);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x02, 0);
+  lcd_send_cmd(0x28);
+  lcd_send_cmd(0x0C);
+  lcd_send_cmd(0x06);
+  lcd_send_cmd(0x01);
+  HAL_Delay(2);
+}
+
+void lcd_write_string(char *str) {
+  while (*str) {
+    lcd_send_data(*str++);
+  }
+}
+
+void lcd_set_cursor(uint8_t row, uint8_t column) {
+    uint8_t address;
+    switch (row) {
+        case 0:
+            address = 0x00;
+            break;
+        case 1:
+            address = 0x40;
+            break;
+        default:
+            address = 0x00;
+    }
+    address += column;
+    lcd_send_cmd(0x80 | address);
+}
+
+void lcd_clear(void) {
+	lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+void lcd_backlight(uint8_t state) {
+  if (state) {
+    backlight_state = 1;
+  } else {
+    backlight_state = 0;
+  }
+}
 
 /* USER CODE END 4 */
 
